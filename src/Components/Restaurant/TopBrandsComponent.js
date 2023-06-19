221740a296ca842ba8bfc0efb81cb796e275bc10
Sahil <sahilladhania5@gmail.com>
2023-06-20 00:17:01 +0530

    Done some styling in Top Brands Component.

--- src/Components/Restaurant/TopBrandsComponent.js
+++ src/Components/Restaurant/TopBrandsComponent.js
@@ -15,7 +15,7 @@ const TopBrandsComponent = () => {
                         <div className='flex fd-c ai-c'>
                             <div className='mb-10'>
                                 <a href="#">
-                                    <img className='h-150 w-a br-80' src="https://b.zmtcdn.com/data/brand_creatives/logos/d0dc2644282f23930e2f7b430a527ddd_1650625613.png" alt="Andra-Gun-Powder-Image" />
+                                    <img className='h-150 w-a br-80 shadow' src="https://b.zmtcdn.com/data/brand_creatives/logos/d0dc2644282f23930e2f7b430a527ddd_1650625613.png" alt="Andra-Gun-Powder-Image" />
                                 </a>
                             </div>
                             <div className='mb-10'>
@@ -32,7 +32,7 @@ const TopBrandsComponent = () => {
                         <div className='flex fd-c ai-c'>
                             <div className='mb-10'>
                                 <a href="#">
-                                    <img className='h-150 w-a br-80' src="https://b.zmtcdn.com/data/brand_creatives/logos/6a11fd0f30c9fd9ceaff2f5b21f61d23_1617187721.png" alt="Burger-King-Image" />
+                                    <img className='h-150 w-a br-80 shadow' src="https://b.zmtcdn.com/data/brand_creatives/logos/6a11fd0f30c9fd9ceaff2f5b21f61d23_1617187721.png" alt="Burger-King-Image" />
                                 </a>
                             </div>
                             <div className='mb-10'>
@@ -49,7 +49,7 @@ const TopBrandsComponent = () => {
                         <div className='flex fd-c ai-c'>
                             <div className='mb-10'>
                                 <a href="#">
-                                    <img className='h-150 w-a br-80' src="https://b.zmtcdn.com/data/brand_creatives/logos/ccfc99a48a81fd2a0092e9c4ad02614c_1575441347.png" alt="Mani's-Dum-Biryani-Image" />
+                                    <img className='h-150 w-a br-80 shadow' src="https://b.zmtcdn.com/data/brand_creatives/logos/ccfc99a48a81fd2a0092e9c4ad02614c_1575441347.png" alt="Mani's-Dum-Biryani-Image" />
                                 </a>
                             </div>
                             <div className='mb-10'>
@@ -66,7 +66,7 @@ const TopBrandsComponent = () => {
                         <div className='flex fd-c ai-c'>
                             <div className='mb-10'>
                                 <a href="#">
-                                    <img className='h-150 w-a br-80' src="https://b.zmtcdn.com/data/brand_creatives/logos/9302c59eca94abbee01aec9acf9305f6_1676520725.png" alt="Subway-Image" />
+                                    <img className='h-150 w-a br-80 shadow' src="https://b.zmtcdn.com/data/brand_creatives/logos/9302c59eca94abbee01aec9acf9305f6_1676520725.png" alt="Subway-Image" />
                                 </a>
                             </div>
                             <div className='mb-10'>
@@ -83,7 +83,7 @@ const TopBrandsComponent = () => {
                         <div className='flex fd-c ai-c'>
                             <div className='mb-10'>
                                 <a href="#">
-                                    <img className='h-150 w-a br-80' src="https://b.zmtcdn.com/data/brand_creatives/logos/239f872f52291fe253a37cc8dd16188f_1663838300.png" alt="Truffles-Image" />
+                                    <img className='h-150 w-a br-80 shadow' src="https://b.zmtcdn.com/data/brand_creatives/logos/239f872f52291fe253a37cc8dd16188f_1663838300.png" alt="Truffles-Image" />
                                 </a>
                             </div>
                             <div className='mb-10'>
@@ -100,7 +100,7 @@ const TopBrandsComponent = () => {
                         <div className='flex fd-c ai-c'>
                             <div className='mb-10'>
                                 <a href="#">
-                                    <img className='h-150 w-a br-80' src="https://b.zmtcdn.com/data/brand_creatives/logos/9742d760cf95e9dbf9b869ca9c753f8f_1613210633.png" alt="Pizza-Hut-Image" />
+                                    <img className='h-150 w-a br-80 shadow' src="https://b.zmtcdn.com/data/brand_creatives/logos/9742d760cf95e9dbf9b869ca9c753f8f_1613210633.png" alt="Pizza-Hut-Image" />
                                 </a>
                             </div>
                             <div className='mb-10'>
