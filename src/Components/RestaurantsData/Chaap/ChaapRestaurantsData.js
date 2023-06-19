275b922239cd665ddaa411b1ebf489e02c46c7b5
Sahil <sahilladhania5@gmail.com>
2023-06-19 14:30:25 +0530

    Done with Restaurants Data.

--- src/Components/RestaurantsData/Chaap/ChaapRestaurantsData.js
+++ src/Components/RestaurantsData/Chaap/ChaapRestaurantsData.js
@@ -1,84 +1,84 @@
 const chaapRestaurants = [
     {
-        imgSrc : "",
-        imgAlt : "",
-        restaurantName : "",
-        cuisine : "",
-        rating : "",
-        deliveryTime : "",
-        priceForTwo : ""
+        imgSrc : "https://b.zmtcdn.com/data/dish_photos/846/80b603aa33ae000b8e534500015f4846.jpg",
+        imgAlt : "Flavours-Of-Chakhna-Image",
+        restaurantName : "Flavours Of Chakhna",
+        cuisine : "Chaap",
+        rating : "4.5",
+        deliveryTime : "40 MINS",
+        priceForTwo : "350"
     },
     {
-        imgSrc : "",
-        imgAlt : "",
-        restaurantName : "",
-        cuisine : "",
-        rating : "",
-        deliveryTime : "",
-        priceForTwo : ""
+        imgSrc : "https://b.zmtcdn.com/data/dish_photos/576/6742942038763f439d017fdfc244d576.jpg",
+        imgAlt : "Jain-Saab-Ki-Malai-Chaap-Image",
+        restaurantName : "Jain Saab Ki Malai Chaap",
+        cuisine : "Chaap",
+        rating : "3.7",
+        deliveryTime : "40 MINS",
+        priceForTwo : "400"
     },
     {
-        imgSrc : "",
-        imgAlt : "",
-        restaurantName : "",
-        cuisine : "",
-        rating : "",
-        deliveryTime : "",
-        priceForTwo : ""
+        imgSrc : "https://b.zmtcdn.com/data/dish_photos/c61/9bc7c47cc96eb37a71360b4515fe1c61.jpg",
+        imgAlt : "Swadesi-Tandoor-Image",
+        restaurantName : "Swadesi Tandoor",
+        cuisine : "Chaap",
+        rating : "3.8",
+        deliveryTime : "30 MINS",
+        priceForTwo : "400"
     },
     {
-        imgSrc : "",
-        imgAlt : "",
-        restaurantName : "",
-        cuisine : "",
-        rating : "",
-        deliveryTime : "",
-        priceForTwo : ""
+        imgSrc : "https://b.zmtcdn.com/data/dish_photos/36b/bf9e5d71d5fe3febf48ed38ba301136b.jpg",
+        imgAlt : "Tikka-Junction-Image",
+        restaurantName : "Tikka Junction",
+        cuisine : "Chaap",
+        rating : "4.3",
+        deliveryTime : "40 MINS",
+        priceForTwo : "300"
     },
     {
-        imgSrc : "",
-        imgAlt : "",
-        restaurantName : "",
-        cuisine : "",
-        rating : "",
-        deliveryTime : "",
-        priceForTwo : ""
+        imgSrc : "https://b.zmtcdn.com/data/dish_photos/005/5410f81a6d793f9743bb1cc616012005.jpg",
+        imgAlt : "Veerji-Malai-Chaap-Waale-Image",
+        restaurantName : "Veer Ji Malai Chaap Wale",
+        cuisine : "Chaap",
+        rating : "4.6",
+        deliveryTime : "45 MINS",
+        priceForTwo : "400"
     },
     {
-        imgSrc : "",
-        imgAlt : "",
-        restaurantName : "",
-        cuisine : "",
-        rating : "",
-        deliveryTime : "",
-        priceForTwo : ""
+        imgSrc : "https://b.zmtcdn.com/data/dish_photos/9af/3f3b8034a6fc42847e4045810fd719af.jpg",
+        imgAlt : "Chawlas-Veg-Kitchen-Image",
+        restaurantName : "Chawlas Veg Kitchen",
+        cuisine : "Chaap",
+        rating : "3.9",
+        deliveryTime : "40 MINS",
+        priceForTwo : "350"
     },
     {
-        imgSrc : "",
-        imgAlt : "",
-        restaurantName : "",
-        cuisine : "",
-        rating : "",
-        deliveryTime : "",
-        priceForTwo : ""
+        imgSrc : "https://b.zmtcdn.com/data/dish_photos/8e0/0e0af82f3a4a146f8090d7a41f16d8e0.jpg",
+        imgAlt : "Kadak-Singh-Da-Dhaba-Image",
+        restaurantName : "Kadak Singh Da Dhaba",
+        cuisine : "Chaap",
+        rating : "4.5",
+        deliveryTime : "35 MINS",
+        priceForTwo : "400"
     },
     {
-        imgSrc : "",
-        imgAlt : "",
-        restaurantName : "",
-        cuisine : "",
-        rating : "",
-        deliveryTime : "",
-        priceForTwo : ""
+        imgSrc : "https://b.zmtcdn.com/data/pictures/chains/0/18511800/1d893fe871c392d9300887006def91db_o2_featured_v2.jpg",
+        imgAlt : "Yellow-Bowl-Image",
+        restaurantName : "The Yellow Bowl",
+        cuisine : "Chaap",
+        rating : "4.3",
+        deliveryTime : "45 MINS",
+        priceForTwo : "500"
     },
     {
-        imgSrc : "",
-        imgAlt : "",
-        restaurantName : "",
-        cuisine : "",
-        rating : "",
-        deliveryTime : "",
-        priceForTwo : ""
+        imgSrc : "https://b.zmtcdn.com/data/dish_photos/79d/960c2ac25c5d4e26aa25abca27b3c79d.jpg",
+        imgAlt : "Punjabi-Junction-Image",
+        restaurantName : "The Punjabi Junction",
+        cuisine : "Chaap",
+        rating : "4.3",
+        deliveryTime : "35 MINS",
+        priceForTwo : "350"
     }
 ]
 
