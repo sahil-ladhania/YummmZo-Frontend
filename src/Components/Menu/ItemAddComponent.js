fb239772c694e94e9972ad81e91a5be392f8364a
Sahil <sahilladhania5@gmail.com>
2023-06-23 19:44:06 +0530

    Implemented Quantity State For Add to Cart.

--- src/Components/Menu/ItemAddComponent.js
+++ src/Components/Menu/ItemAddComponent.js
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../../Styles/Main.css'
 import '../../Styles/Buttons.css'
 
 const ItemAddComponent = () => {
+    const [quantity , setQuantity] = useState(1);
+    const [isAddedToCart , setIsAddedToCart] = useState(false);
+
+    const handleQuantity = () => {
+        setIsAddedToCart(true);
+    }
+    const handleAdd = () => {
+        setQuantity(quantity + 1);
+    }
+    const handleLess = () => {
+        if (quantity > 1){
+            setQuantity(quantity - 1);
+        }
+        else{
+            setQuantity(1);
+            setIsAddedToCart(false);
+        }
+    }
     return (
         <div>
             <>
                 {/* Main Component */}
                 <div>
                     {/* Add Button Section */}
-                    <div>
-                        <button className='h-40 w-100 b-1-t-s br-5 fs-r'>Add</button>
-                    </div>
+                        <div>
+                            {
+                                isAddedToCart ? 
+                                <div className='h-40 w-100 flex ai-c jc-sb'>
+                                    <button className='h-40 w-30 b-1-t-s br-5 fs-r pointer' onClick={handleLess}>-</button>
+                                    <input className='h-40 w-30 b-1-t-s br-5 fs-r shadow' type="button" value={quantity} />
+                                    <button className='h-40 w-30 b-1-t-s br-5 fs-r pointer' onClick={handleAdd}>+</button>
+                                </div>
+                                :
+                                <button onClick={handleQuantity} className='h-40 w-100 b-1-t-s br-5 fs-r pointer'>Add to Cart</button>
+                            }
+                        </div>
                 </div>
             </>
         </div>
