9c20e5c73cdd6d39226c0ea2ef3180f9b81ecc7a
Sahil <sahilladhania5@gmail.com>
2023-06-23 08:35:54 +0530

    Implemented Favourite Restaurant State.

--- src/Components/Menu/LinkFavSearchComponent.js
+++ src/Components/Menu/LinkFavSearchComponent.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { UilHeart } from '@iconscout/react-unicons'
 import { UilSearch } from '@iconscout/react-unicons'
 import { Link } from 'react-router-dom';
 
 const LinkFavSearchComponent = () => {
+    const [favourites, setFavorites] = useState(false);
+
+    const handleFavourite = () => {
+        setFavorites(!favourites);
+    }
+
     return (
         <div>
             <>      
@@ -18,7 +24,14 @@ const LinkFavSearchComponent = () => {
                     {/* Icon's Section */}
                     <div>
                         <span className='flex ai-c'>
-                            <UilHeart className='h-20 w-a mr-10 pointer'/>
+                            <p onClick={handleFavourite}>
+                                {
+                                            favourites ? 
+                                                <p className='h-20 w-a mr-10 pointer'>❤️</p>
+                                                :
+                                                <UilHeart className='h-20 w-a mr-10 pointer'/>
+                                }
+                            </p>
                             <UilSearch className='h-20 w-a ml-10 pointer'/>
                         </span>
                     </div>
