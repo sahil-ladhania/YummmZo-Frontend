4c34613f3b045ade49eb5b130597d18ce555e977
Sahil <sahilladhania5@gmail.com>
2023-06-30 20:27:41 +0530

    Added Items Data for one Restaurant.

---
+++ src/Components/RestaurantItemDetailsData/BiryaniByKilo.js
@@ -0,0 +1,40 @@
+const BiryaniByKiloData = [
+    {
+        name : "Biryani By Kilo",
+        cuisine : "Biryani",
+        items : [
+            {
+                id: 1,
+                itemName:"Chicken Dum Biryani",
+                price: "Rs.250",
+                description:"This is a Chicken Biryani."
+            },
+            {
+                id: 2,
+                itemName:"Mutton Dum Biryani",
+                price: "Rs.350",
+                description:"This is a Mutton Biryani."
+            },
+            {
+                id: 3,
+                itemName:"Veg Dum Biryani",
+                price: "Rs.200",
+                description:"This is a Veg Biryani."
+            },
+            {
+                id: 4,
+                itemName:"Phirni",
+                price: "Rs.500",
+                description:"This is a Phirni."
+            },
+            {
+                id: 5,
+                itemName:"Kebeb",
+                price: "Rs.400",
+                description:"This is a Kebab."
+            }
+        ]
+    }
+]
+
+export default BiryaniByKiloData;
