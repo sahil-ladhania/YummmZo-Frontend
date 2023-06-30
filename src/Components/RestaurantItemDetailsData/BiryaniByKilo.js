5dd7c0f989c8a5527a6bb73e0931ae6c0029b5f0
Sahil <sahilladhania5@gmail.com>
2023-06-30 20:46:35 +0530

    Changed Items in Data.

--- src/Components/RestaurantItemDetailsData/BiryaniByKilo.js
+++ src/Components/RestaurantItemDetailsData/BiryaniByKilo.js
@@ -5,33 +5,33 @@ const BiryaniByKiloData = [
         items : [
             {
                 id: 1,
-                itemName:"Chicken Dum Biryani",
-                price: "Rs.250",
-                description:"This is a Chicken Biryani."
+                itemName:"Veg Hyderabadi Dum Biryani - [1 Kg] Serves 2-3",
+                price: "Rs.695",
+                description:"Serves 1 | Fresh vegetables are marinated in a mixture of freshly ground BBK spices & layered with long grain basmati rice in the handi and slow cooked in the the handi. Each biryani order is individually cooked in hand made clay handi and served to you in the same earthenware. BBK biryani is layered, and Masala is at bottom of Handi. To enjoy authentic flavours mix it well. 1 Kg [Serves 2 - 3 People]."
             },
             {
                 id: 2,
-                itemName:"Mutton Dum Biryani",
-                price: "Rs.350",
-                description:"This is a Mutton Biryani."
+                itemName:"Peshawari Chole Biryani [1/2 Kg] Serves 1-2",
+                price: "Rs.399",
+                description:"Serves 1 | [Served with Burani Raita] Traditional Peshwari chole, paneer cubes & cashew nut with freshly ground BBK spices & layered with long grain basmati rice in the handi and slow cooked in the handi. A must try for vegetarians. BBK biryani is layered, and Masala is at bottom of Handi. To enjoy authentic flavours mix it well. [1/2kg serves 1-2]"
             },
             {
                 id: 3,
-                itemName:"Veg Dum Biryani",
-                price: "Rs.200",
-                description:"This is a Veg Biryani."
+                itemName:"Veg Platter",
+                price: "Rs.610",
+                description:"Serves 4 | [4pcs] Veg Galouti, [4pcs] Kaju Seekh, [3pcs] Crispy Corn Stick, [4pcs] Kasturi Paneer. Served with a Spicy Dip, Mint Chutney & Onion salad. + 2 Complimentary Imli Adraki Lemonade."
             },
             {
                 id: 4,
-                itemName:"Phirni",
-                price: "Rs.500",
-                description:"This is a Phirni."
+                itemName:"Salan [200ml]",
+                price: "Rs.65",
+                description:"Serves 1"
             },
             {
                 id: 5,
-                itemName:"Kebeb",
-                price: "Rs.400",
-                description:"This is a Kebab."
+                itemName:"Paneer 65 [250 gms]",
+                price: "Rs.399",
+                description:"Crispy fried cheese cubes marinated in an Hyderabadi masala and tossed in a tangy sauce with spices & curry leaves. Surely a vegetarian delight"
             }
         ]
     }
