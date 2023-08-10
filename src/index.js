f09a2f83936e6b079224ebf121281ceeb5fa178e
Sahil <sahilladhania5@gmail.com>
2023-08-10 20:19:40 +0530

    Started Implementing auth0.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
@@ -9,7 +10,15 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
     <React.StrictMode>
-    <App />
+      <Auth0Provider
+      domain="yummmzo.us.auth0.com"
+      clientId="ZE9HOMrYqMUWq6eaFkyY3Q8shdHSVSBQ"
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+      >
+        <App />
+      </Auth0Provider>
     </React.StrictMode>
   </Router>
 );
