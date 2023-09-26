848687a7e06c477f535611071fb189612cb3f9a7
Sahil <sahilladhania5@gmail.com>
2023-09-27 00:51:38 +0530

    Deleted One Sort Button.

--- src/Components/Restaurant/RestaurantFilterComponent.js
+++ src/Components/Restaurant/RestaurantFilterComponent.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { MdFavorite } from "react-icons/md";
 import { IoTime } from "react-icons/io5";
 import { AiFillStar } from "react-icons/ai";
 import { BsFillArrowUpCircleFill } from "react-icons/bs";
@@ -20,12 +19,8 @@ const RestaurantFilterComponent = () => {
                     <div className='flex'>
                         {/* Filter Options Section */}
                         <div className='flex ai-c ml-10 p-10 b-1-d8d8d8-s br-5'>
-                            <MdFavorite className='h-15 w-a c-3d3d3d'/>
-                            <a className='fs-r td-n c-3d3d3d fs-s' href="#">Favourites</a>
-                        </div>
-                        <div className='flex ai-c ml-10 p-10 b-1-d8d8d8-s br-5'>
                             <IoTime className='h-15 w-a c-3d3d3d'/>
-                            <a className='fs-r td-n c-3d3d3d fs-s' href="#">Delivery Time</a>
+                            <a className='fs-r td-n c-3d3d3d fs-s' href="#">Fast Delivery</a>
                         </div>
                         <div className='flex ai-c ml-10 p-10 b-1-d8d8d8-s br-5'>
                             <AiFillStar className='h-15 w-a c-3d3d3d'/>
