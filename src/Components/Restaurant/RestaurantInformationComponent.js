817fcf86a51d4f16c379fcc43a3c2c84a2947798
Sahil <sahilladhania5@gmail.com>
2023-09-28 20:35:55 +0530

    Done Some Changes.

--- src/Components/Restaurant/RestaurantInformationComponent.js
+++ src/Components/Restaurant/RestaurantInformationComponent.js
@@ -1,43 +1,8 @@
 import React, {useState} from 'react'
-import { registerRestaurant } from '../../Services/RestaurantService';
 
 const RestaurantInformationComponent = () => {
     // Defining State Variables.
-    const [restaurantFormData , setRestaurantFormData] = useState({
-        restaurantName : '',
-        restaurantCuisine : '',
-        deliveryTime: '',
-        priceForTwo : '',
-        imageURL : '',
-        restaurantCompleteAddress : '',
-        mobileNumberAtRestaurant : '',
-        mobileNumberOfOwner : '',
-        restaurantOwnerName : '',
-        restaurantOwnerEmailAddress : '',
-        rating : ''
-    })
     // Handler Functions.
-    const handleChange = (e) => {
-        setRestaurantFormData({...restaurantFormData , [e.target.name] : e.target.value});
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(restaurantFormData);
-        registerRestaurant(restaurantFormData);
-        setRestaurantFormData({
-            restaurantName : '',
-            restaurantCuisine : '',
-            deliveryTime: '',
-            priceForTwo : '',
-            imageURL : '',
-            restaurantCompleteAddress : '',
-            mobileNumberAtRestaurant : '',
-            mobileNumberOfOwner : '',
-            restaurantOwnerName : '',
-            restaurantOwnerEmailAddress : '',
-            rating : ''
-        })
-    }
     return (
         <div>
             <>
@@ -48,7 +13,7 @@ const RestaurantInformationComponent = () => {
                         Restaurant Information
                     </div>
                     {/* Form Section */}
-                    <form action="" onSubmit={handleSubmit}>
+                    <form action="">
                         {/* Restaurant Details */}
                         <div className=''>
                             <div className='mb-20'>
@@ -59,8 +24,6 @@ const RestaurantInformationComponent = () => {
                             </div>
                             <div className='mb-20 flex fd-c'>
                                 <input 
-                                onChange={handleChange}
-                                value={restaurantFormData.restaurantName}
                                 className='h-40 w-500 p-0-10 mb-20 b-1-t-s br-5 bc-ws' 
                                 type="text"
                                 name="restaurantName" 
@@ -68,48 +31,36 @@ const RestaurantInformationComponent = () => {
                                 placeholder='Resturant Name'
                                 />
                                 <input 
-                                onChange={handleChange}
-                                value={restaurantFormData.restaurantCuisine}
                                 className='h-40 w-500 p-0-10 mb-20 b-1-t-s br-5 bc-ws' 
                                 type="text" 
                                 name="restaurantCuisine" 
                                 id="" 
                                 placeholder='Restaurant Cuisine'/>
                                 <input 
-                                onChange={handleChange}
-                                value={restaurantFormData.deliveryTime}
                                 className='h-40 w-500 p-0-10 mb-20 b-1-t-s br-5 bc-ws' 
                                 type="text" 
                                 name="deliveryTime" 
                                 id="" 
                                 placeholder='Delivery Time'/>
                                 <input 
-                                onChange={handleChange}
-                                value={restaurantFormData.priceForTwo}
                                 className='h-40 w-500 p-0-10 mb-20 b-1-t-s br-5 bc-ws' 
                                 type="text" 
                                 name="priceForTwo" 
                                 id="" 
                                 placeholder='Price For Two'/>
                                 <input 
-                                onChange={handleChange}
-                                value={restaurantFormData.imageUrl}
                                 className='h-40 w-500 p-0-10 mb-20 b-1-t-s br-5 bc-ws' 
                                 type="text" 
                                 name="imageUrl" 
                                 id="" 
                                 placeholder='Enter Image URL'/>
                                 <input 
-                                onChange={handleChange}
-                                value={restaurantFormData.restaurantCompleteAddress}
                                 className='h-40 w-500 p-0-10 mb-20 b-1-t-s br-5 bc-ws' 
                                 type="text" 
                                 name="restaurantCompleteAddress" 
                                 id="" 
                                 placeholder='Restaurant Complete Address'/>
                                 <input 
-                                onChange={handleChange}
-                                value={restaurantFormData.rating}
                                 className='h-40 w-500 p-0-10 mb-20 b-1-t-s br-5 bc-ws' 
                                 type="text" 
                                 name="rating" 
@@ -127,8 +78,6 @@ const RestaurantInformationComponent = () => {
                             </div>
                             <div className='mb-20'>
                                 <input 
-                                onChange={handleChange}
-                                value={restaurantFormData.mobileNumberAtRestaurant}
                                 className='h-40 w-500 p-0-10 mb-20 b-1-t-s br-5 bc-ws' 
                                 type="number" 
                                 name="mobileNumberAtRestaurant" 
@@ -146,24 +95,18 @@ const RestaurantInformationComponent = () => {
                             </div>
                             <div className='mb-20 flex fd-c'>
                                 <input 
-                                onChange={handleChange}
-                                value={restaurantFormData.mobileNumberOfOwner}
                                 className='h-40 w-500 p-0-10 mb-20 b-1-t-s br-5 bc-ws' 
                                 type="number" 
                                 name="mobileNumberOfOwner" 
                                 id="" 
                                 placeholder='Mobile Number Of Owner'/>
                                 <input 
-                                onChange={handleChange}
-                                value={restaurantFormData.restaurantOwnerName}
                                 className='h-40 w-500 p-0-10 mb-20 b-1-t-s br-5 bc-ws' 
                                 type="text" 
                                 name="restaurantOwnerName" 
                                 id="" 
                                 placeholder='Restaurant Owner Full Name'/>
                                 <input 
-                                onChange={handleChange}
-                                value={restaurantFormData.restaurantOwnerEmailAddress}
                                 className='h-40 w-500 p-0-10 mb-20 b-1-t-s br-5 bc-ws' 
                                 type="email" 
                                 name="restaurantOwnerEmailAddress" 
--- src/Services/RestaurantService.js
+++ src/Services/RestaurantService.js
@@ -10,19 +10,6 @@ export const getAllRestaurants = () => {
             throw new Error(`Error Fetching Restaurants Data : ${error}`);
         })
 }
-export const registerRestaurant = (restaurantData) => {
-    const endpoint = "http://localhost:81/restaurants";
-    return axios.post(endpoint, restaurantData)
-        .then((response) => {
-            return response.data;
-        })
-        .catch((error) => {
-            if (error.response) {
-                console.log("Registration Error:", error.response.data);
-            }
-            throw new Error(`Error Registering Restaurant: ${error}`);
-    });
-};
 export const getRestaurantById = (restaurantId) => {
     const endpoint = `http://localhost:81/restaurants/${restaurantId}`;
     return axios.get(endpoint , restaurantId)
