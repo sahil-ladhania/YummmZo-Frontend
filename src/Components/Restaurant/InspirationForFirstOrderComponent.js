7c773bbd825c746c6338dbc35f37869f0aa9e49d
Sahil <sahilladhania5@gmail.com>
2023-10-22 03:22:23 +0530

    Done Some Styling.

--- src/Components/Restaurant/InspirationForFirstOrderComponent.js
+++ src/Components/Restaurant/InspirationForFirstOrderComponent.js
@@ -9,11 +9,11 @@ const InspirationForFirstOrderComponent = ({ cuisines }) => {
                     <div>
                         <span className='text-3xl font-roboto text-yummmzo-color'>Inspiration for your first order</span>
                     </div>
-                    <div className='flex cursor-pointer'>
+                    <div className='flex cursor-pointer w-full bg-secondary'>
                         {cuisines.map((cuisine, index) => (
-                            <div key={index} className='flex mb-5 mt-5 flex-col items-center mr-2 ml-2'>
-                                <img className='h-20 mb-5 rounded-full' src={cuisine.imageURL} alt="" />
-                                <a className='text-sm font-medium font-roboto text-yummmzo-color' href="#">{cuisine.cuisineName}</a>
+                            <div key={index} className='h-40 w-36 flex mb-5 mt-5 flex-col items-center justify-center mr-2 ml-2'>
+                                <img className='h-16 mb-5 rounded-full' src={cuisine.imageURL} alt="" />
+                                <a className='text-sm font-medium font-roboto text-yummmzo-color text-center' href="#">{cuisine.cuisineName}</a>
                             </div>
                         ))}
                     </div>
--- src/Components/Restaurant/TopBrandsComponent.js
+++ src/Components/Restaurant/TopBrandsComponent.js
@@ -11,7 +11,7 @@ const TopBrandsComponent = () => {
                         <h2 className='text-3xl font-roboto text-yummmzo-color'>Top Brands For You</h2>
                     </div>
                     {/* Brands Section */}
-                    <div className='flex items-center justify-between w-full'>
+                    <div className='flex items-center justify-evenly w-full'>
                         <div className='flex flex-col items-center h-40 justify-between'>
                             <div className=''>
                                 <a href="#">
